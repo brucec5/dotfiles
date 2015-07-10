663d1d52ffb06a0e04ff8b3d3ebea0df4a233824
Chris Bruce <chris@futureadvisor.com>
2015-07-10 09:44:24 -0700

    Move rails logging in irb into a callable method

--- irbrc/rails.rb
+++ irbrc/rails.rb
@@ -1,6 +1,16 @@
 # TODO: Actually learn the best way to configure irb with rails if rails exists
 if ENV['RAILS_ENV']
+  existing_conf = IRB.conf[:IRB_RC]
+
+  def enable_active_record_logging!
+    # Log SQL queries to stdout
+    ActiveRecord::Base.connection.instance_variable_set :@logger, Logger.new(STDOUT)
+  end
+
   IRB.conf[:IRB_RC] = Proc.new do
+    if existing_conf
+      existing_conf.call
+    end
 
     # Let you use Model[id] to find by id
     class ActiveRecord::Base
@@ -9,8 +19,7 @@ if ENV['RAILS_ENV']
       end
     end
 
-    # Log SQL queries to stdout
-    ActiveRecord::Base.connection.instance_variable_set :@logger, Logger.new(STDOUT)
+    enable_active_record_logging!
 
     # For marshalling session data
     def load_session(session_data)
