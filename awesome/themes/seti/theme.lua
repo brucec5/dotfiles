c7ea3b2627e43a55d3edfe7b8370ab110cc6c393
Chris Bruce <aprofessionalemailaddress@gmail.com>
2016-12-10 15:09:31 -0800

    Customize awesome config, integrate lain widgets, and add a new theme

---
+++ awesome/themes/seti/theme.lua
@@ -0,0 +1,123 @@
+-- {{{ Main
+theme = {}
+-- }}}
+
+-- {{{ Styles
+theme.font      = "clean 10"
+theme.xfld_font = '-*-clean-medium-r-*-*-15-*-*-*-*-*-*-*'
+theme.tasklist_disable_icon = true
+
+-- {{{ Colors
+theme.fg_normal  = "#cacecd"
+theme.fg_focus   = "#43a5d5"
+theme.fg_urgent  = "#111213"
+theme.fg_accent  = "#8b57b5"
+theme.fg_ok      = "#8ec43d"
+theme.fg_warn    = "#e0c64f"
+theme.fg_error   = "#c22832"
+theme.bg_normal  = "#111213"
+theme.bg_focus   = "#303233"
+theme.bg_urgent  = "#c22832"
+theme.bg_systray = theme.bg_normal
+-- }}}
+
+-- {{{ Borders
+theme.border_width  = 1
+theme.border_normal = "#111213"
+theme.border_focus  = "#43a5d5"
+theme.border_marked = "#c22832"
+-- }}}
+
+-- {{{ Titlebars
+theme.titlebar_bg_focus  = "#303233"
+theme.titlebar_bg_normal = "#111213"
+-- }}}
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- Example:
+--theme.taglist_bg_focus = "#CC9393"
+-- }}}
+
+-- {{{ Widgets
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.fg_widget        = "#AECF96"
+--theme.fg_center_widget = "#88A175"
+--theme.fg_end_widget    = "#FF5656"
+--theme.bg_widget        = "#494B4F"
+--theme.border_widget    = "#3F3F3F"
+-- }}}
+
+-- {{{ Mouse finder
+theme.mouse_finder_color = "#c22832"
+-- mouse_finder_[timeout|animate_timeout|radius|factor]
+-- }}}
+
+-- {{{ Menu
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_height = 15
+theme.menu_width  = 100
+-- }}}
+
+-- {{{ Icons
+-- {{{ Taglist
+theme.taglist_squares_sel   = "~/.config/awesome/themes/seti/taglist/squarefz.png"
+theme.taglist_squares_unsel = "~/.config/awesome/themes/seti/taglist/squarez.png"
+--theme.taglist_squares_resize = "false"
+-- }}}
+
+-- {{{ Misc
+theme.awesome_icon           = "~/.config/awesome/themes/seti/awesome-icon.png"
+theme.menu_submenu_icon      = "/usr/share/awesome/themes/default/submenu.png"
+-- }}}
+
+-- {{{ Layout
+theme.layout_tile       = "~/.config/awesome/themes/seti/layouts/tile.png"
+theme.layout_tileleft   = "~/.config/awesome/themes/seti/layouts/tileleft.png"
+theme.layout_tilebottom = "~/.config/awesome/themes/seti/layouts/tilebottom.png"
+theme.layout_tiletop    = "~/.config/awesome/themes/seti/layouts/tiletop.png"
+theme.layout_fairv      = "~/.config/awesome/themes/seti/layouts/fairv.png"
+theme.layout_fairh      = "~/.config/awesome/themes/seti/layouts/fairh.png"
+theme.layout_spiral     = "~/.config/awesome/themes/seti/layouts/spiral.png"
+theme.layout_dwindle    = "~/.config/awesome/themes/seti/layouts/dwindle.png"
+theme.layout_max        = "~/.config/awesome/themes/seti/layouts/max.png"
+theme.layout_fullscreen = "~/.config/awesome/themes/seti/layouts/fullscreen.png"
+theme.layout_magnifier  = "~/.config/awesome/themes/seti/layouts/magnifier.png"
+theme.layout_floating   = "~/.config/awesome/themes/seti/layouts/floating.png"
+-- }}}
+
+-- {{{ Titlebar
+theme.titlebar_close_button_focus  = "~/.config/awesome/themes/seti/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = "~/.config/awesome/themes/seti/titlebar/close_normal.png"
+
+theme.titlebar_ontop_button_focus_active  = "~/.config/awesome/themes/seti/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active = "~/.config/awesome/themes/seti/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive  = "~/.config/awesome/themes/seti/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive = "~/.config/awesome/themes/seti/titlebar/ontop_normal_inactive.png"
+
+theme.titlebar_sticky_button_focus_active  = "~/.config/awesome/themes/seti/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active = "~/.config/awesome/themes/seti/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive  = "~/.config/awesome/themes/seti/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive = "~/.config/awesome/themes/seti/titlebar/sticky_normal_inactive.png"
+
+theme.titlebar_floating_button_focus_active  = "~/.config/awesome/themes/seti/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active = "~/.config/awesome/themes/seti/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive  = "~/.config/awesome/themes/seti/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive = "~/.config/awesome/themes/seti/titlebar/floating_normal_inactive.png"
+
+theme.titlebar_maximized_button_focus_active  = "~/.config/awesome/themes/seti/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active = "~/.config/awesome/themes/seti/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = "~/.config/awesome/themes/seti/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = "~/.config/awesome/themes/seti/titlebar/maximized_normal_inactive.png"
+-- }}}
+-- }}}
+
+return theme
