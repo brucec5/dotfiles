43169fcbf2abf273bcd9393cd819aed8c1dee237
Chris Bruce <christopher.bruce@blackrock.com>
2022-03-21 13:55:01 -0700

    Make HS screens more dynamic (still requires a config reload)

--- hammerspoon/mouse.lua
+++ hammerspoon/mouse.lua
@@ -1,21 +1,3 @@
---local mapping = { 2, 1, 3 }
--- changing this while working from home, since my monitor setup is different
--- TODO: add ability to switch monitor schemes dynamically?
--- TODO: consider sharing the mapping object between mouse and screens modules
-local mapping = { 1, 2 }
-
-for i, screenIndex in ipairs(mapping) do
-  hs.hotkey.bind({"ctrl", "alt", "cmd"}, tostring(i), function()
-    local screen = hs.screen.allScreens()[screenIndex]
-
-    if screen then
-      local rect = screen:fullFrame()
-      local center = hs.geometry.rectMidPoint(rect)
-      hs.mouse.setAbsolutePosition(center)
-    end
-  end)
-end
-
 local mouseCircle = nil
 local mouseCircleTimer = nil
 
--- hammerspoon/screens.lua
+++ hammerspoon/screens.lua
@@ -1,20 +1,51 @@
---local mapping = { 2, 1, 3 }
--- changing this while working from home, since my monitor setup is different
--- TODO: add ability to switch monitor schemes dynamically?
-local mapping = { 1, 2 }
+local logger = hs.logger.new("screens", "info")
 
-for i, screenIndex in ipairs(mapping) do
+local watcher = hs.screen.watcher.new(function()
+  logger.i("Screens changed!")
+  hs.alert("whoa! screens changed!", {}, hs.screen.mainScreen())
+end)
+
+watcher:start()
+
+local monitors = hs.screen.screenPositions()
+
+local monitorCount = 0
+local minXPosition = 100
+for screen, pos in pairs(monitors) do
+  monitorCount = monitorCount + 1
+  if pos.x < minXPosition then
+    minXPosition = pos.x
+  end
+end
+
+-- Offset makes it so the leftmost monitor is index 1, even if the main monitor is in the middle or elsewhere.
+local offset = -minXPosition + 1
+
+logger.i("we have " .. monitorCount .. " monitors, min x is " .. minXPosition)
+
+for screen, pos in pairs(monitors) do
+  logger.i("Binding for screen " .. hs.inspect.inspect(screen))
+
+  local i = pos.x + offset
   hs.hotkey.bind({"ctrl", "alt"}, tostring(i), function()
+    logger.d("Moving window to screen " .. hs.inspect.inspect(screen))
     local win = hs.window.focusedWindow()
-    local screen = hs.screen.allScreens()[screenIndex]
 
     win:moveToScreen(screen)
     win:setFrame(screen:frame())
   end)
+
+  hs.hotkey.bind({"ctrl", "alt", "cmd"}, tostring(i), function()
+    logger.d("Moving mouse to screen " .. hs.inspect.inspect(screen))
+
+    local rect = screen:fullFrame()
+    local center = hs.geometry.rectMidPoint(rect)
+    hs.mouse.absolutePosition(center)
+  end)
 end
 
 hs.hotkey.bind({"ctrl", "alt", "cmd"}, "s", function()
-  for i, screen in ipairs(hs.screen.allScreens()) do
-    hs.alert("Screen " .. mapping[i], {}, screen)
+  for screen, pos in pairs(monitors) do
+    hs.alert("Screen " .. (pos.x + offset), {}, screen)
   end
 end)
