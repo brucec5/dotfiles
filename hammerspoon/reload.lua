8d913630a79581db469f749a78cdb240b9024c69
Chris Bruce <christopher.bruce@blackrock.com>
2022-03-21 13:56:08 -0700

    Add HS binding for config reload (faster than relaunching when that's applicable)

--- hammerspoon/reload.lua
+++ hammerspoon/reload.lua
@@ -16,5 +16,6 @@ watcher = hs.pathwatcher.new(
 ):start()
 
 hs.hotkey.bind({"cmd", "ctrl", "alt", "shift"}, "r", function() hs.relaunch() end)
+hs.hotkey.bind({"cmd", "ctrl", "alt"}, "r", function() hs.reload() end)
 
 hs.alert.show("Hammerspoon Config Loaded!")
